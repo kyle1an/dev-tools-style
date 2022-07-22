08e32970b9a4642d36f7ae0349236655bfd29af5
Mitchell <50689806+kyle1an@users.noreply.github.com>
2022-07-22 20:55:38 +0800

    refactor: use less

--- devtools.js
+++ devtools.js
@@ -1,20 +1,20 @@
-const styles = ['dist/style.css', 'dist/gutterStyle.css'];
-const fonts = ['dist/fonts.css'];
+const styles = ['style.css', 'gutterStyle.css']
+const fonts = ['fonts.css']
 
-console.log('devtools.js loaded');
+console.log('devtools.js loaded')
 
 function applyStyle(paths) {
   for (const path of paths) {
-    const x = new XMLHttpRequest();
+    const x = new XMLHttpRequest()
     x.open('GET', path);
-    x.onload = () => chrome.devtools.panels.applyStyleSheet(x.responseText);
-    x.send();
+    x.onload = () => chrome.devtools.panels.applyStyleSheet(x.responseText)
+    x.send()
   }
-  console.log('stylesApplied', paths);
+  console.log('stylesApplied', paths)
 }
 
-applyStyle([...styles, ...fonts]);
+applyStyle([...styles, ...fonts])
 
 chrome.devtools.panels.sources.onSelectionChanged.addListener(() => {
-  applyStyle([styles[1]]);
-});
+  applyStyle([styles[1]])
+})
